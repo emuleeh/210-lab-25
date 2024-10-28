a152eba5b24994fe363a857378b8a84111b40808
Emily <emuleehw@gmail.com>
2024-10-27 20:36:40 -0700

    Third Commit

--- COMSC-Lab-25/main.cpp
+++ COMSC-Lab-25/main.cpp
@@ -8,13 +8,47 @@ using namespace std;
 using  namespace std:: chrono;
 
 
+float readVector(vector<string> &v);
+float readList(list<string> &l);
+float readSet(set<string> &s);
+
+
 
 int main() {
+    vector<string> v;
+    list<string> l;
+    set<string> s;
     
+    
+    
+    
+    cout << "Operation" << "\tVector" << "\tList" << "\tSet" << endl;
+    cout << "Read" << "\t" <<
     
     
     return 0;
 }
+
+float readVector(vector<string> &v) {
+    //read into vector
+    auto start = high_resolution_clock::now;
+    
+    ifstream fin;
+    fin.open("codes.txt");
+    if (fin.is_open())
+    {
+        string line;
+        while (fin >> line)
+        {
+            v.push_back(line);
+        }
+        fin.close();
+    }
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    return duration.count();
+}
+
 
 /* syntax examples:
  auto start = high_resolution_clock::now()

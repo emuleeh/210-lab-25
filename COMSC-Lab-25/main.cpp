461ae1d485104acf0d76b7bf2cdc156d4d0d8ab9
Emily <emuleehw@gmail.com>
2024-10-27 17:38:01 -0700

    Initial Commit

---
+++ COMSC-Lab-25/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMSC-Lab-25
+//
+//  Created by E on 10/27/24.
+//  Copyright © 2024 E. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
